2dbe2ab1aa901d63d086a1cbef175892a6bcc65c
asifshaj98 <asif1232@hotmail.co.uk>
2023-10-18 14:25:32 +0100

    transform function

--- etl.py
+++ etl.py
@@ -21,3 +21,29 @@ def extract_data() -> dict:
         return None
 
 # %%
+def transform(data: dict) -> pd.DataFrame:
+    """ Transforms the dataset into the desired structure and filters."""
+    try:
+        df = pd.DataFrame(data)
+        print(f"Total Number of universities from API: {len(data)}")
+
+        # Filter universities in California
+        df_california = df[df["name"].str.contains("California", case=False)]
+        print(f"Number of universities in California: {len(df_california)}")
+
+        # Join domains and web_pages lists into comma-separated strings
+        df_california['domains'] = df_california['domains'].apply(lambda x: ','.join(map(str, x)))
+        df_california['web_pages'] = df_california['web_pages'].apply(lambda x: ','.join(map(str, x)))
+
+        # Reset index and select desired columns
+        df_transformed = df_california.reset_index(drop=True)[["domains", "country", "web_pages", "name"]]
+        return df_transformed
+    except KeyError as e:
+        print("Error: The input data is not in the expected format.")
+        return None
+    except Exception as e:
+        print("An error occurred during data transformation:", str(e))
+        return None
+
+
+# %%
