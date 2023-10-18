52895d0c8e839498fc7a2bdf32e56891e17ad5f6
asifshaj98 <asif1232@hotmail.co.uk>
2023-10-18 14:09:48 +0100

    extract code

--- etl.py
+++ etl.py
@@ -7,4 +7,17 @@ import requests
 from sqlalchemy import create_engine
 
 # %%
+def extract_data() -> dict:
+    """ This function extracts data from the universities API."""
+    API_URL = "http://universities.hipolabs.com/search?country=United+States"
 
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()  # Check for HTTP errors
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print("Error during API request:", str(e))
+        return None
+
+# %%
